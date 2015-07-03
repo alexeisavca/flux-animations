0cae6de0906aefec128c9beaf1872111022eca3c
Alexei <savca.alexei@gmail.com>
2015-07-03 21:26:36 +0300

    Let there be Webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require('path');
+module.exports = {
+    entry: {
+        main: "./index.jsx"
+    },
+    output: {
+        path: './',
+        filename: "index.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.jsx?$/, loaders: ['jsx?harmony'] }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    }
+};
