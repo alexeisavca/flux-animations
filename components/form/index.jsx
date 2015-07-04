fdbf44ab048d190f9f5731ea8649e503b382219a
Alexei <savca.alexei@gmail.com>
2015-07-05 01:58:06 +0300

    Handling currentAnimation and currentTarget updates

--- components/form/index.jsx
+++ components/form/index.jsx
@@ -4,20 +4,28 @@ var {List} = require('immutable');
 
 module.exports = Form;
 class Form extends PureRenderComponent {
+    updateCurrentAnimation(event){
+        this.props.setCurrentAnimation(event.target.value);
+    }
+
+    updateCurrentTarget(event){
+        this.props.setCurrentTarget(event.target.value);
+    }
+
     render() {
-        var {animations, targets} = this.props;
+        var {animations, targets, currentAnimation, currentTarget} = this.props;
         return (
             <div className="col-md-12">
                 <form className="form-inline">
                     <div className="form-group">
-                        <select className="form-control">
+                        <select className="form-control" value={currentAnimation} onChange={this.updateCurrentAnimation.bind(this)}>
                             {animations.map(animation => (
                                 <option value={animation.get('slug')} key={animation.get('slug')}>{animation.get('name')}</option>
                             ))}
                         </select>
                     </div>
                     <div className="form-group">
-                        <select className="form-control">
+                        <select className="form-control" value={currentTarget} onChange={this.updateCurrentTarget.bind(this)}>
                             {targets.map(target => (
                                 <option value={target.get('slug')} key={target.get('slug')}>{target.get('name')}</option>
                             ))}
