3f9ddedb12f2a07a5d50130d34aed94ebc6d40be
Alexei <savca.alexei@gmail.com>
2015-07-05 00:24:37 +0300

    Added flux, mock data, rendering the <DemoApp/>

---
+++ index.jsx
@@ -0,0 +1,33 @@
+var React = require('react');
+var DemoApp = require('./components/demo-app');
+var {List, Map} = require('immutable');
+var Flux = require('./simple-flux')['default'];
+var animations = List([
+    Map({
+        name: "Fade",
+        slug: "fade"
+    })
+]);
+
+var targets = List([
+    Map({
+        name: "Box",
+        slug: "box"
+    }),
+    Map({
+        name: "Image",
+        slug: "image"
+    })
+]);
+
+var flux = new Flux();
+var globalStateStore = flux.store("globalStateStore");
+globalStateStore.setCurrentAnimation("fade");
+
+flux.setOnStoreUpdateListener(function(){
+    React.render(<DemoApp
+        animations={animations}
+        targets={targets}
+        currentAnimation={globalStateStore.getCurrentAnimation()}
+    />, document.getElementById('the-container'));
+});
