5fd8bf8f2d351082241e4c19e132e84ba55e1a46
Alexei <savca.alexei@gmail.com>
2015-07-05 01:58:30 +0300

    Added actions prop type. Passing new props to <Form/>

--- components/demo-app/index.jsx
+++ components/demo-app/index.jsx
@@ -6,24 +6,33 @@ var {List} = require('immutable');
 module.exports = DemoApp;
 class DemoApp extends PureRenderComponent {
     render() {
-        var {animations, targets} = this.props;
+        var {animations, targets, currentAnimation, currentTarget, actions} = this.props;
         return (
             <div className="row">
                 <div className="col-md-12 text-center">
                     <a href="https://github.com/alexeisavca/flux-animations">View source on github.</a>
                 </div>
-                <Form animations={animations} targets={targets}/>
+                <Form
+                    animations={animations}
+                    targets={targets}
+                    currentAnimation={currentAnimation}
+                    currentTarget={currentTarget}
+                    setCurrentAnimation={actions.setCurrentAnimation.bind(this.props.actions)}
+                    setCurrentTarget={actions.setCurrentTarget.bind(this.props.actions)}
+                />
             </div>
         )
     }
 }
 
-var {instanceOf, string, func} = React.PropTypes;
+var {instanceOf, string, func, shape} = React.PropTypes;
 Form.propTypes = {
     animations: instanceOf(List),
     target: instanceOf(List),
     currentAnimation: string.isRequired,
-    setCurrentAnimation: func.isRequired,
     currentTarget: string.isRequired,
-    setCurrentTarget: func.isRequired
+    actions: shape({
+        setCurrentAnimation: func.isRequired,
+        setCurrentTarget: func.isRequired
+    }).isRequired
 };
