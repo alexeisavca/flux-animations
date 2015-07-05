a27dd1178a7d18cb71827d45ce8932e25180a377
Alexei <savca.alexei@gmail.com>
2015-07-05 18:16:18 +0300

    Fade options component

---
+++ components/fade-options/index.jsx
@@ -0,0 +1,60 @@
+var React = require('react');
+var PureRenderComponent = require('../pure-render-component');
+module.exports = FadeOptions;
+class FadeOptions extends PureRenderComponent {
+    updateAnimationOptions(optionName, event){
+        this.props.updateAnimationOption('fade', optionName, parseFloat(event.target.value));
+    }
+
+    render() {
+        var {from, to, duration} = this.props;
+        return (
+            <span>
+                <div className="form-group">
+                    <label>From:</label>
+                    <input
+                        type="number"
+                        className="form-control"
+                        value={from}
+                        onChange={this.updateAnimationOptions.bind(this, 'from')}
+                        step="0.1"
+                        min="0"
+                        max="1"
+                    />
+                </div>
+                &nbsp;
+                <div className="form-group">
+                    <label>To:</label>
+                    <input
+                        type="number"
+                        className="form-control"
+                        value={to}
+                        onChange={this.updateAnimationOptions.bind(this, 'to')}
+                        step="0.1"
+                        min="0"
+                        max="1"
+                    />
+                </div>
+                &nbsp;
+                <div className="form-group">
+                    <label>Duration:</label>
+                    <input
+                        type="number"
+                        className="form-control"
+                        value={duration}
+                        onChange={this.updateAnimationOptions.bind(this, 'duration')}
+                        min="0"
+                    />
+                </div>
+            </span>
+        )
+    }
+}
+var {number, func} = React.PropTypes;
+var requiredNumber = number.isRequired;
+FadeOptions.propTypes = {
+    from: requiredNumber,
+    to: requiredNumber,
+    duration: requiredNumber,
+    updateAnimationOption: func.isRequired
+};
