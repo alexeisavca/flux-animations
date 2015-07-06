4468eb9a2432dfb60c685e6837b949a4eb15ab2b
Alexei <savca.alexei@gmail.com>
2015-07-06 15:06:32 +0300

    Added a paragraph, a new animation target

--- components/demo-app/index.jsx
+++ components/demo-app/index.jsx
@@ -1,12 +1,13 @@
 var React = require('react');
 var PureRenderComponent = require('../pure-render-component');
 var Form = require('../form');
-var {List} = require('immutable');
+var {List, Map} = require('immutable');
 
 module.exports = DemoApp;
 class DemoApp extends PureRenderComponent {
     render() {
-        var {animations, targets, currentAnimation, currentTarget, actions, animationMode, imageStyle} = this.props;
+        var {animations, targets, currentAnimation, currentTarget, actions, animationMode, imageStyle,
+            paragraphStyle} = this.props;
         return (
             <div className="row">
                 <div className="col-md-12 text-center">
@@ -24,8 +25,21 @@ class DemoApp extends PureRenderComponent {
                     setAnimationMode={actions.setAnimationMode.bind(this.props.actions)}
                     animationActions={actions.animations}
                 />
-                <div className="col-md-12" style={{marginTop: 10}}>
-                    <img src="https://placekitten.com/1000/600" alt="" style={imageStyle}/>
+                <div className="col-md-6" style={{marginTop: 10}}>
+                    <img src="http://placekitten.com/400/600" alt="" style={imageStyle.toJS()}/>
+                </div>
+                <div className="col-md-6" style={{marginTop: 10}}>
+                    <p style={paragraphStyle.toJS()}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id tristique purus. Morbi tincidunt est vitae massa volutpat posuere. Mauris ut faucibus nisi. Sed quis semper velit. Aliquam elementum mi eu enim pulvinar mattis. In porta in turpis non pretium. Aenean at facilisis diam. Morbi non iaculis eros.
+
+                        Aliquam condimentum iaculis dolor, vel aliquet enim pretium non. Duis vel libero vel dolor sagittis suscipit vel non libero. Suspendisse venenatis at velit vel tempor. Etiam et elementum massa. Pellentesque auctor lacus vulputate congue blandit. Phasellus consequat, est eu consectetur porttitor, lacus est dignissim dolor, sit amet pulvinar eros risus id massa. Sed nisi tortor, lacinia sit amet nisi non, commodo sagittis turpis. Aenean pretium lectus at laoreet iaculis. Phasellus porttitor sapien id hendrerit rutrum. Integer vel ullamcorper nunc. Vestibulum dui ipsum, sollicitudin ac odio eget, tincidunt ultricies sem. Donec suscipit eros orci, vel egestas sem sollicitudin tempor. Maecenas placerat ligula eget ipsum rutrum, ut suscipit nisl fringilla.
+
+                        Sed rutrum nec nulla a ultrices. Morbi egestas quam in sapien vulputate, at ornare est placerat. Praesent sit amet arcu a justo elementum dictum. Ut lacinia vel sem ut tincidunt. Sed vel facilisis augue. Vestibulum feugiat diam at efficitur luctus. Sed vel ultricies purus. Vestibulum tincidunt, nisi sit amet accumsan pharetra, velit quam auctor magna, eu viverra nunc ligula hendrerit elit. Proin in tempor nisl, id feugiat ipsum. Morbi consequat neque purus, a feugiat turpis suscipit elementum. Sed rhoncus dictum rutrum. Sed et nulla non leo placerat elementum sed ut risus.
+
+                        Pellentesque eu rutrum mauris. Nunc pulvinar aliquet urna vitae gravida. Suspendisse eget sapien ipsum. In porttitor orci ut tincidunt facilisis. Donec aliquam ornare aliquam. Nunc odio ex, tempus ut efficitur vitae, vehicula ut ipsum. Vestibulum consequat rhoncus sapien, at fermentum libero vulputate vel. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris aliquet blandit nulla, non interdum ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed interdum ipsum dolor, eu placerat odio semper a. Pellentesque sit amet est non massa ornare congue. Duis ac tempus orci. Quisque tincidunt, lacus in semper vulputate, nulla sapien tempor tortor, sed posuere ante risus varius nisl. Vestibulum tristique felis quam, feugiat iaculis nisl rhoncus ac.
+
+                        Praesent in elit accumsan, rhoncus ligula et, vehicula nunc. Vivamus porttitor enim et aliquet cursus. In pretium metus nisl, vel mattis leo hendrerit ac. Phasellus porttitor nisl quis ornare vestibulum. Nullam sed leo nisi. Praesent commodo dolor in enim dictum blandit. In nibh ligula, lobortis quis egestas ac, maximus at tellus. Proin placerat sed mauris id suscipit. Nulla blandit consequat diam, aliquam tempus sapien. Donec iaculis dui tortor, in molestie purus placerat nec. Nunc rhoncus molestie turpis, at lobortis mauris venenatis sed. Duis hendrerit efficitur nisi id ullamcorper. In in augue purus. Proin consectetur libero sit amet tortor congue, ut auctor lacus faucibus.
+                    </p>
                 </div>
             </div>
         )
@@ -34,6 +48,7 @@ class DemoApp extends PureRenderComponent {
 
 var {instanceOf, string, func, shape, oneOf, object} = React.PropTypes;
 var requiredFunc = func.isRequired;
+var requiredMap = instanceOf(Map).isRequired;
 DemoApp.propTypes = {
     animations: instanceOf(List),
     target: instanceOf(List),
@@ -50,5 +65,6 @@ DemoApp.propTypes = {
             resize: requiredFunc
         }).isRequired
     }).isRequired,
-    imageStyle: object.isRequired
+    imageStyle: requiredMap,
+    paragraphStyle: requiredMap
 };
